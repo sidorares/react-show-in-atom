23cd114a0c704bcf7685b32b040d5c4d8038dd22
Andrey Sidorov <sidorares@yandex.com>
2017-03-11 23:10:14 +1100

    use function.toString() instead of big string with source

--- locate-source.js
+++ locate-source.js
@@ -2,36 +2,33 @@
 
 const port = process.env.PORT || 3222
 
-const script = `
-  (function() {
-    var keyboardState = {};
-    document.addEventListener('mousedown', function(e) {
-      var dom = e.target;
-      for (var key in dom) {
-        if (key.startsWith("__reactInternalInstance$")) {
-          window.__react$0 = dom[key]._currentElement;
-          var compInternals = dom[key]._currentElement;
-          if (compInternals && e.metaKey) {
-            var source = compInternals._source;
+var injectClickHandler = function() {
+  var source = null;
+  document.addEventListener('mousedown', function(e) {
+    var targetElement = e.target;
+    for (var key in targetElement) {
+      if (key.startsWith('__reactInternalInstance$')) {
+        var compInternals = targetElement[key]._currentElement;
+        if (compInternals && e.metaKey) {
+          source = compInternals._source;
+          while (!source) {
+            compInternals = compInternals._owner._currentElement;
+            source = compInternals._source
+          }
+          if (compInternals && e.shiftKey) {
+            compInternals = compInternals._owner._currentElement;
+            source = compInternals._source
             while (!source) {
               compInternals = compInternals._owner._currentElement;
               source = compInternals._source
             }
-            if (compInternals && e.shiftKey) {
-              compInternals = compInternals._owner._currentElement;
-              source = compInternals._source
-              while (!source) {
-                compInternals = compInternals._owner._currentElement;
-                source = compInternals._source
-              }
-            }
-            fetch('http://localhost:${port}/source?filename=' + source.fileName + '&line=' + source.lineNumber);
           }
+          fetch('http://localhost:${port}/go-to-element-source?filename=' + source.fileName + '&line=' + source.lineNumber);
         }
       }
-    });
-  })();
-  `
+    }
+  });
+};
 
 const express = require('express')
 const app = express()
@@ -48,21 +45,29 @@ const messageAtom = m => {
   s.end()
 }
 
+const openInAtom = (fileName, lineNumber) => {
+  messageAtom({
+    pathsToOpen: [`${fileName}:${lineNumber}`]
+  })
+}
+
 const parse = require('url').parse
 const path = require('path')
 
-app.get('/open-in-editor.js', (req, res) => { res.end(script) })
-app.get('/source', (req, res) => {
+app.get('/open-in-editor.js', (req, res) => {
+  res.end(`(${injectClickHandler.toString()})()`)
+})
+
+app.get('/go-to-element-source', (req, res) => {
   res.end('ok')
   const q = parse(req.url, true).query
-  const filename = path.resolve(__dirname, q.filename)
+  const fileName = path.resolve(__dirname, q.filename)
+  const lineNumber = q.line
   res.end('ok')
-  if (typeof q.line == 'undefined' || q.line === 'undefined') {
+  if (typeof lineNumber === 'undefined' || lineNumber === 'undefined') {
     return
   }
-  messageAtom({
-    pathsToOpen: [`${filename}:${q.line}${typeof q.column != 'undefined' ? (':' + q.column) : '' }`]
-  })
+  openInAtom(fileName, lineNumber)
 })
 
 http.createServer(app).listen(port)
