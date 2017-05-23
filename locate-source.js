9559a9334e1db2acf04cc0b0b1b67fb64d628b47
Andrey Sidorov <sidorares@yandex.com>
2017-05-23 16:17:13 +1000

    fix in inected script port

--- locate-source.js
+++ locate-source.js
@@ -1,73 +1,79 @@
 #!/usr/bin/env node
 
-const port = process.env.PORT || 3222
+const port = process.env.PORT || 3222;
 
-var injectClickHandler = function() {
+var injectClickHandler = function(port) {
   var source = null;
   document.addEventListener('mousedown', function(e) {
     var targetElement = e.target;
     for (var key in targetElement) {
       if (key.startsWith('__reactInternalInstance$')) {
         var compInternals = targetElement[key]._currentElement;
-        if (compInternals && e.metaKey) {
+        if (compInternals && e.altKey) {
           source = compInternals._source;
           while (!source) {
             compInternals = compInternals._owner._currentElement;
-            source = compInternals._source
+            source = compInternals._source;
           }
           if (compInternals && e.shiftKey) {
             compInternals = compInternals._owner._currentElement;
-            source = compInternals._source
+            source = compInternals._source;
             while (!source) {
               compInternals = compInternals._owner._currentElement;
-              source = compInternals._source
+              source = compInternals._source;
             }
           }
-          fetch('http://localhost:${port}/go-to-element-source?filename=' + source.fileName + '&line=' + source.lineNumber);
+          fetch(
+            `http://localhost:${port}/go-to-element-source?filename=` + source.fileName + '&line=' + source.lineNumber
+          );
         }
       }
     }
   });
 };
 
-const express = require('express')
-const app = express()
-const http = require('http')
+const express = require('express');
+const app = express();
+const http = require('http');
 
 const net = require('net')
 const sock = process.env.ATOM_SOCK || require('child_process').execSync(`lsof -c Atom | grep sock | awk '{print $NF}'`).toString().split('\n').join('')
 
+// windows: try to do something similar to above automaticly, possibly using https://technet.microsoft.com/en-us/sysinternals/dd581625.aspx
+// const sock = '\\\\.\\pipe\\atom-1.18.0-beta0-YXNpZG9ybzE=-x64-sock'
+
 console.log(`Using socket ${sock} to talk to Atom`)
 
+
 const messageAtom = m => {
-  const s = net.connect(sock)
-  s.write(JSON.stringify(m))
-  s.end()
-}
+  const s = net.connect(sock);
+  s.write(JSON.stringify(m));
+  s.end();
+};
 
 const openInAtom = (fileName, lineNumber) => {
   messageAtom({
     pathsToOpen: [`${fileName}:${lineNumber}`]
-  })
-}
+  });
+};
 
-const parse = require('url').parse
-const path = require('path')
+const parse = require('url').parse;
+const path = require('path');
 
 app.get('/open-in-editor.js', (req, res) => {
-  res.end(`(${injectClickHandler.toString()})()`)
-})
+  res.end(`(${injectClickHandler.toString()})(${port})`);
+});
 
 app.get('/go-to-element-source', (req, res) => {
-  res.end('ok')
-  const q = parse(req.url, true).query
-  const fileName = path.resolve(__dirname, q.filename)
-  const lineNumber = q.line
-  res.end('ok')
+  res.end('ok');
+  const q = parse(req.url, true).query;
+  const fileName = path.resolve(__dirname, q.filename);
+  const lineNumber = q.line;
+  res.end('ok');
   if (typeof lineNumber === 'undefined' || lineNumber === 'undefined') {
-    return
+    return;
   }
-  openInAtom(fileName, lineNumber)
-})
+  openInAtom(fileName, lineNumber);
+});
 
-http.createServer(app).listen(port)
+http.createServer(app).listen(port);
